fe7324065ceab0cf42218c2771be82be2290013c
Christiaan Benedictus <chris@combat.nl>
2023-10-26 17:55:12 +0200

    Added license and small import update

--- packages/vfs-driver-local/test/LocalVfsDriver.test.ts
+++ packages/vfs-driver-local/test/LocalVfsDriver.test.ts
@@ -1,8 +1,27 @@
+// Copyright (c) 2023 Combat Jongerenmarketing en -communicatie B.V.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import { IVfsDriver } from "@acte-de-presence/vfs";
+import { afterAll, beforeAll, describe, expect, test } from 'bun:test';
 import * as fs from 'fs';
 import * as path from 'path';
 import { LocalVfsDriver } from '../src/LocalVfsDriver';
-import { DirectoryContents, DirectoryMetadata, FileContents, FileMetadata, IVfsDriver, IVfsDriverResult } from "@acte-de-presence/vfs";
-import { afterAll, beforeAll, describe, expect, setSystemTime, test } from 'bun:test';
 
 const rootFolder = path.join(import.meta.dir, 'test-folder');
 const driver: IVfsDriver = new LocalVfsDriver(rootFolder);
